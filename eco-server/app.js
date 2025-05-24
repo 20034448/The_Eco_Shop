415681570fe9befc0cd0a23e1cfcc7345c393248
ThiagoAbbudCoutinho <t.abbud@gmail.com>
2025-05-24 12:53:56 +0100

    server working

--- eco-server/app.js
+++ eco-server/app.js
@@ -29,13 +29,17 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server its on 3000 ${port}`);
 });
 
 module.exports = app;
